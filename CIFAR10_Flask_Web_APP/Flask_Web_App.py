b49f53398e4ef258d6628c900d3529311701f5cf
“chiragbhuvaneshwara” <chiragbhuvaneshwar@gmail.com>
2019-07-03 00:19:11 +0200

    finished the website visualization

--- CIFAR10_Flask_Web_APP/Flask_Web_App.py
+++ CIFAR10_Flask_Web_APP/Flask_Web_App.py
@@ -4,15 +4,22 @@ import os
 import scipy.misc
 from models import *
 from matplotlib.pyplot import imread
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import torch
+import torch.nn.functional as F
+import numpy as np
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device: %s'%device)
 
 def loadModelAndTest():
-    
+    labels = ["airplane", 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
     global secure_filename
     #Reading the image file from the path it was saved in previously.
-    inputImageDir = os.path.join(app.root_path, secure_filename)
+    inputImageDir = os.path.join(app.root_path,'static', secure_filename)
     img = imread(inputImageDir)
     img = torch.tensor(img, device=device).float()
     img = img.unsqueeze(0)
@@ -28,6 +35,17 @@ def loadModelAndTest():
         model.eval()                
         images = torch.cat((img,img),0)
         outputs = model(images)
+        outputs = F.softmax(outputs)
+        output_prob = outputs.data.numpy()[0]
+        data = {'Class':labels, 'Probability':output_prob}
+        data_list = [[labels[i], output_prob[i]] for i in range(len(labels))]
+        df = pd.DataFrame(data)
+        plt.figure(figsize=(10,12))
+        sns.barplot(x="Probability", y="Class", data=df, order=labels, palette='Set2');
+        plt.xlabel("Probability Score")
+        plt.ylabel('Class')
+        plt.title('Class Prediction Output')
+        plt.savefig(os.path.join(app.root_path,'static','plot.png'))
         _, predicted_classes = torch.max(outputs.data, 1)
         predicted_class = predicted_classes[0]
         predicted_class = labels[int(predicted_class)]
@@ -36,7 +54,8 @@ def loadModelAndTest():
         # s = "file://" + inputImageDir
         # print('#####################################################')
         # print(s)
-        return flask.render_template(template_name_or_list="prediction_result.html", predicted_class=predicted_class, inputImageDir= secure_filename)
+        return flask.render_template(template_name_or_list="prediction_result.html", predicted_class=predicted_class, inputImageDir= secure_filename,\
+            data_list = data_list, labels=labels, probs=list([str(i) for i in output_prob]))
 
     else:
         # If the image dimensions do not match the CIFAR10 specifications, then an HTML page is rendered to show the problem.
@@ -48,7 +67,7 @@ def upload_image():
     if flask.request.method == "POST":#Checking of the HTTP method initiating the request is POST.
         img_file = flask.request.files["image_file"]#Getting the file name to get uploaded.
         secure_filename = werkzeug.secure_filename(img_file.filename)#Getting a secure file name. It is a good practice to use it.
-        img_path = os.path.join(app.root_path, secure_filename)#Preparing the full path under which the image will get saved.
+        img_path = os.path.join(app.root_path,'static', secure_filename)#Preparing the full path under which the image will get saved.
         img_file.save(img_path)#Saving the image in the specified path.
         print("Image uploaded successfully.")
       
